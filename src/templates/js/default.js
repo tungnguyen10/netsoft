b8de6648231da3dcb77f95949c231a69fa61b0d1
Nguyen Thanh Tung <thanhtung.airtickets@gmail.com>
2020-10-26 01:29:18 +0700

    master

--- src/templates/js/default.js
+++ src/templates/js/default.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-
+  $(window).scroll(function() {
+		$(this).scrollTop() > 200 ? $('.backtop').addClass('show') : $('.backtop').removeClass('show')
+	});
+	$('.backtop').click(function() {
+		return $("body,html").animate({
+			scrollTop: 0
+		}, 800), !1
+	});
+  $('a.btn-support').click(function(e){
+    e.stopPropagation();
+    $('.support-content').slideToggle();
+  });
 });
