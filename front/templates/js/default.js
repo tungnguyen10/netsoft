55cb1fc3c96ed9815d5d5dbafdbb1a8d8e2686ac
Nguyen Thanh Tung <thanhtung.airtickets@gmail.com>
2021-01-09 10:32:11 +0700

    09/01/2021

--- front/templates/js/default.js
+++ front/templates/js/default.js
@@ -1,61 +1,94 @@
 $(document).ready(function () {
-	$(window).scroll(function () {
-		$(this).scrollTop() > 200 ? $('.backtop').addClass('show') : $('.backtop').removeClass('show')
-	});
-	/*** back to top ***/
-	$('.backtop').click(function () {
-		return $("body,html").animate({
-			scrollTop: 0
-		}, 800), !1
-	});
-	/*** btn support left ***/
-	$('a.btn-support').click(function (e) {
-		e.stopPropagation();
-		$('.support-content').slideToggle();
-	});
+  $(window).scroll(function () {
+    $(this).scrollTop() > 200
+      ? $(".backtop").addClass("show")
+      : $(".backtop").removeClass("show");
+  });
+  /*** back to top ***/
+  $(".backtop").click(function () {
+    return (
+      $("body,html").animate(
+        {
+          scrollTop: 0,
+        },
+        800
+      ),
+      !1
+    );
+  });
+  /*** btn support left ***/
+  $("a.btn-support").click(function (e) {
+    e.stopPropagation();
+    $(".support-content").slideToggle();
+  });
 
-	/*** Footer mobile ***/
-	var ww = $(window).width();
-	if (ww < 768) {
-		$('.foo-title').on('click', function () {
-			$(this).next('.foo-content').slideToggle('fast');
-		});
-	} else {
-		$('.foo-content').show();
-	}
+  /*** Footer mobile ***/
+  var ww = $(window).width();
+  if (ww < 768) {
+    $(".foo-title").on("click", function () {
+      $(this).next(".foo-content").slideToggle("fast");
+    });
+  } else {
+    $(".foo-content").show();
+  }
 
-	/** menu mobile */
-	$('.menubutton').click(function (e) {
-		e.stopPropagation();
-		$('.wrapmenu_right').toggleClass('open_sidebar_menu');
-		$('.opacity_menu').toggleClass('open_opacity');
-		$('body').toggleClass('open_menu');
-	});
-	$('.opacity_menu').click(function (e) {
-		$('.wrapmenu_right').removeClass('open_sidebar_menu');
-		$('.opacity_menu').removeClass('open_opacity');
-		$('body').toggleClass('open_menu');
-	});
-	$(".menubar_pc").click(function () {
-		$('.wrapmenu_full').slideToggle('fast');
-		$('.wrapmenu_full, .cloed').toggleClass('open_menu');
-		$('.dqdt-sidebar, .open-filters').removeClass('openf')
-	});
-	$(".cloed").click(function () {
-		$(this).toggleClass('open_menu');
-		$('.wrapmenu_full').slideToggle('fast');
-	});
-	$(".opacity_menu").click(function () {
-		$('.opacity_menu').removeClass('open_opacity');
-	});
-	if ($('.dqdt-sidebar').hasClass('openf')) {
-		$('.wrapmenu_full').removeClass('open_menu');
-	}
-	$('.ul_collections li > .fa').click(function () {
-		$(this).parent().toggleClass('current');
-		$(this).toggleClass('fa-plus fa-minus');
-		$(this).next('ul').slideToggle("fast");
-		$(this).next('div').slideToggle("fast");
-	});
-
+  /** menu mobile */
+  $(".menubutton").click(function (e) {
+    e.stopPropagation();
+    $(".wrapmenu_right").toggleClass("open_sidebar_menu");
+    $(".opacity_menu").toggleClass("open_opacity");
+    $("body").toggleClass("open_menu");
+  });
+  $(".opacity_menu").click(function (e) {
+    $(".wrapmenu_right").removeClass("open_sidebar_menu");
+    $(".opacity_menu").removeClass("open_opacity");
+    $("body").toggleClass("open_menu");
+  });
+  $(".menubar_pc").click(function () {
+    $(".wrapmenu_full").slideToggle("fast");
+    $(".wrapmenu_full, .cloed").toggleClass("open_menu");
+    $(".dqdt-sidebar, .open-filters").removeClass("openf");
+  });
+  $(".cloed").click(function () {
+    $(this).toggleClass("open_menu");
+    $(".wrapmenu_full").slideToggle("fast");
+  });
+  $(".opacity_menu").click(function () {
+    $(".opacity_menu").removeClass("open_opacity");
+  });
+  if ($(".dqdt-sidebar").hasClass("openf")) {
+    $(".wrapmenu_full").removeClass("open_menu");
+  }
+  $(".ul_collections li > .fa").click(function () {
+    $(this).parent().toggleClass("current");
+    $(this).toggleClass("fa-plus fa-minus");
+    $(this).next("ul").slideToggle("fast");
+    $(this).next("div").slideToggle("fast");
+  });
 });
+if ($("#index").length > 0) {
+  $(".owl-ebook").owlCarousel({
+		loop: true,
+		autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 4,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+	});
+	$('.whychose dt').click(function () { 
+		$(this).next().slideToggle(500).addClass("active");
+	});
+  
+}
